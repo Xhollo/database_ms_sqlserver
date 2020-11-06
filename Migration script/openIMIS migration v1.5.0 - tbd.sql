f8681e29bc4be3e46d816ebd9328fd47bfe07db8
hirensoni913 <hirensoni913@gmail.com>
2020-11-06 17:02:03 +0300

    uspCreateEnrollmentXML modified to accommodate Vulnerability

--- Migration script/openIMIS migration v1.5.0 - tbd.sql
+++ Migration script/openIMIS migration v1.5.0 - tbd.sql
@@ -251,3 +251,67 @@ BEGIN
 
 END
 GO
+
+ALTER PROCEDURE [dbo].[uspCreateEnrolmentXML]
+(
+	@FamilyExported INT = 0 OUTPUT,
+	@InsureeExported INT = 0 OUTPUT,
+	@PolicyExported INT = 0 OUTPUT,
+	@PremiumExported INT = 0 OUTPUT
+)
+AS
+BEGIN
+	SELECT
+	(SELECT * FROM (SELECT F.FamilyId,F.InsureeId, I.CHFID , F.LocationId, F.Poverty FROM tblInsuree I 
+	INNER JOIN tblFamilies F ON F.FamilyID=I.FamilyID
+	WHERE F.FamilyID IN (SELECT FamilyID FROM tblInsuree WHERE isOffline=1 AND ValidityTo IS NULL GROUP BY FamilyID) 
+	AND I.IsHead=1 AND F.ValidityTo IS NULL
+	UNION
+SELECT F.FamilyId,F.InsureeId, I.CHFID , F.LocationId, F.Poverty
+	FROM tblFamilies F 
+	LEFT OUTER JOIN tblInsuree I ON F.insureeID = I.InsureeID AND I.ValidityTo IS NULL
+	LEFT OUTER JOIN tblPolicy PL ON F.FamilyId = PL.FamilyID AND PL.ValidityTo IS NULL
+	LEFT OUTER JOIN tblPremium PR ON PR.PolicyID = PL.PolicyID AND PR.ValidityTo IS NULL
+	WHERE F.ValidityTo IS NULL 
+	AND (F.isOffline = 1 OR I.isOffline = 1 OR PL.isOffline = 1 OR PR.isOffline = 1)	
+	GROUP BY F.FamilyId,F.InsureeId,F.LocationId,F.Poverty,I.CHFID) aaa	
+	FOR XML PATH('Family'),ROOT('Families'),TYPE),
+	
+	(SELECT * FROM (
+	SELECT I.InsureeID,I.FamilyID,I.CHFID,I.LastName,I.OtherNames,I.DOB,I.Gender,I.Marital,I.IsHead,I.passport,I.Phone,I.CardIssued,NULL EffectiveDate, I.Vulnerability
+	FROM tblInsuree I
+	LEFT OUTER JOIN tblInsureePolicy IP ON IP.InsureeId=I.InsureeID
+	WHERE I.ValidityTo IS NULL AND I.isOffline = 1
+	AND IP.ValidityTo IS NULL 
+	GROUP BY I.InsureeID,I.FamilyID,I.CHFID,I.LastName,I.OtherNames,I.DOB,I.Gender,I.Marital,I.IsHead,I.passport,I.Phone,I.CardIssued, I.Vulnerability
+	)xx
+	FOR XML PATH('Insuree'),ROOT('Insurees'),TYPE),
+
+	(SELECT P.PolicyID,P.FamilyID,P.EnrollDate,P.StartDate,P.EffectiveDate,P.ExpiryDate,P.PolicyStatus,P.PolicyValue,P.ProdID,P.OfficerID, P.PolicyStage
+	FROM tblPolicy P 
+	LEFT OUTER JOIN tblPremium PR ON P.PolicyID = PR.PolicyID
+	INNER JOIN tblFamilies F ON P.FamilyId = F.FamilyID
+	WHERE P.ValidityTo IS NULL 
+	AND PR.ValidityTo IS NULL
+	AND F.ValidityTo IS NULL
+	AND (P.isOffline = 1 OR PR.isOffline = 1)
+	FOR XML PATH('Policy'),ROOT('Policies'),TYPE),
+	(SELECT Pr.PremiumId,Pr.PolicyID,Pr.PayerID,Pr.Amount,Pr.Receipt,Pr.PayDate,Pr.PayType
+	FROM tblPremium Pr INNER JOIN tblPolicy PL ON Pr.PolicyID = PL.PolicyID
+	INNER JOIN tblFamilies F ON F.FamilyId = PL.FamilyID
+	WHERE Pr.ValidityTo IS NULL 
+	AND PL.ValidityTo IS NULL
+	AND F.ValidityTo IS NULL
+	AND Pr.isOffline = 1
+	FOR XML PATH('Premium'),ROOT('Premiums'),TYPE)
+	FOR XML PATH(''), ROOT('Enrolment')
+	
+	
+	SELECT @FamilyExported = ISNULL(COUNT(*),0)	FROM tblFamilies F 	WHERE ValidityTo IS NULL AND isOffline = 1
+	SELECT @InsureeExported = ISNULL(COUNT(*),0) FROM tblInsuree I WHERE I.ValidityTo IS NULL AND I.isOffline = 1
+	SELECT @PolicyExported = ISNULL(COUNT(*),0)	FROM tblPolicy P WHERE ValidityTo IS NULL AND isOffline = 1
+	SELECT @PremiumExported = ISNULL(COUNT(*),0)	FROM tblPremium Pr WHERE ValidityTo IS NULL AND isOffline = 1
+END
+
+
+GO
